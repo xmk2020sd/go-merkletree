c14f6f94ce9c63a4e6cad514bdf3489ec497139f
xmk2020sd <82273410+xmk2020sd@users.noreply.github.com>
2024-09-29 17:25:26 +0800

    Update merkletree_test.go

--- merkletree_test.go
+++ merkletree_test.go
@@ -20,8 +20,8 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/wealdtech/go-merkletree/blake2b"
-	"github.com/wealdtech/go-merkletree/keccak256"
+	"github.com/xmk2020sd/go-merkletree/blake2b"
+	"github.com/xmk2020sd/go-merkletree/keccak256"
 )
 
 // _byteArray is a helper to turn a string in to a byte array
